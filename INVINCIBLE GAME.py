bc113e77b136e39ee3e0ddcf688ddc9fc1052298
RickMorgan149 <dreethigas@gmail.com>
2025-06-18 15:24:09 -0300

    Update INVINCIBLE GAME.py

--- INVINCIBLE GAME.py
+++ INVINCIBLE GAME.py
@@ -4,7 +4,7 @@ import pygame
 pygame.init()
 tela=pygame.display.set_mode((800,600))
 tela.fill((255,0,0))
-icon=pygame.image.load('data\MARK.jpg')
+icon=pygame.image.load('MARK.jpg')
 pygame.display.set_icon(icon) 
 
 pygame.display.update()
